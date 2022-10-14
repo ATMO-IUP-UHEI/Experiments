9022c933a278b55f68942d5688009555698083ce
r-maiwald <r.maiwald@stud.uni-heidelberg.de>
2022-10-14 12:51:05 +0200

    Add MC compatibility for Sensors

--- experiments/shared/sensors.py
+++ experiments/shared/sensors.py
@@ -19,6 +19,14 @@ class Sensors:
                 seed: (1) seed for random number generator
                 time: (1) number of hourly measurements
                 height: (0) height level of the sensors
+
+        Examples
+        --------
+        For new number of sensors
+
+        >>> sensors = Sensors(config)
+        >>> sensors.create_grid(new_n_sensors)
+        >>> sensors.get_index(compute_new=True)
         """
         self.rng = np.random.default_rng(config.get("seed", 1))
 
@@ -26,22 +34,24 @@ class Sensors:
 
         self.n_sensors = config["n_sensors"]
         self.height = config.get("height", 0)
-        noise = config["noise"]
-        if isinstance(noise, dict):
-            pass
-        else:
-            self.noise = xr.DataArray(
-                data=np.tile(noise, [self.n_sensors,self.time]),
-                dims=["sensor", "time_measurement"],
-            )
+        self.std_config = config["noise"]
+        self.set_std(self.std_config)
 
         # Add support for MC
         self.create_grid(self.n_sensors)
 
         self.index = None
 
+    def set_std(self, std):
+        self.std = std
+
+    def set_n_sensors(self, n_sensors):
+        self.n_sensors = n_sensors
+        self.create_grid(n_sensors)
+        self.index = None
+
     def create_grid(self, n_sensors):
-        ny = int(np.sqrt(n_sensors))
+        ny = int(np.ceil(np.sqrt(n_sensors)))
         nx = int(np.ceil(n_sensors / ny))
 
         dx = GRAL.nx // nx
@@ -51,26 +61,32 @@ class Sensors:
             np.arange(0.5 * dy, dy * ny, dy, dtype=int),
         )
 
-    def get_index(self, compute_new=False):
-        if (self.index is None) or compute_new:
-            if self.n_sensors == (self.xmesh.shape[0] * self.xmesh.shape[1]):
-                self.index = (
-                    self.xmesh.flatten(),
-                    self.ymesh.flatten(),
-                    np.repeat(self.height, self.n_sensors),
-                )
-            else:
-                print("Error: not same shape")
+    def get_sample_ids(self, n_sub_sample=None):
+        sample_ids = np.arange(self.n_sensors)
+        # Sample from sensors
+        if n_sub_sample is not None:
+            self.rng.shuffle(sample_ids)
+            sample_ids = np.sort(sample_ids[:n_sub_sample])
+        return sample_ids
+
+    def get_index(self, n_sub_sample=None):
+        if (self.index is None) or (n_sub_sample is not None):
+            sample_ids = self.get_sample_ids(n_sub_sample)
+
+            self.index = (
+                self.xmesh.flatten()[sample_ids[:n_sub_sample]],
+                self.ymesh.flatten()[sample_ids[:n_sub_sample]],
+                np.repeat(self.height, self.n_sensors)[sample_ids],
+            )
         return self.index
 
-    def get_noise(self):
-        mean = np.zeros_like(self.noise)
-        std = self.noise
+    def get_noise(self, n_sub_sample=None):
+        if n_sub_sample is None:
+            n_sub_sample = self.n_sensors
+        mean = np.zeros_like(self.std)
         noise = self.rng.normal(
-            loc=mean, scale=std #, size=(self.n_sensors, self.time)
+            loc=mean, scale=self.std, size=(self.n_sensors, self.time)
         )
-        xr_noise = xr.DataArray(
-            data=noise,
-            dims=self.noise.dims
-        )
-        return xr_noise
+        xr_noise = xr.DataArray(data=noise, dims=["sensor", "time_measurement"])
+        sample_ids = self.get_sample_ids(n_sub_sample)
+        return xr_noise.isel(sensor=sample_ids)
--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -13,10 +13,20 @@ def test_sensors():
 
     config_with_optional = config | {"seed": 1, "time": 2, "height": 1}
 
-    sensors = Sensors(config)
-    sensors.get_index()
-    sensors.get_noise()
+    for config in [config, config_with_optional]:
+        sensors = Sensors(config)
+        sensors.get_index()
+        sensors.get_noise()
 
-    sensors = Sensors(config_with_optional)
-    sensors.get_index()
-    sensors.get_noise()
+        new_n_sensors = 12
+        sensors.set_n_sensors(n_sensors=new_n_sensors)
+        index = sensors.get_index()
+        assert len(index[0]) == new_n_sensors
+        noise = sensors.get_noise()
+        assert len(noise["sensor"]) == new_n_sensors
+
+        n_sub_sample = 5
+        index = sensors.get_index(n_sub_sample)
+        assert len(index[0]) == n_sub_sample
+        noise = sensors.get_noise(n_sub_sample)
+        assert len(noise["sensor"]) == n_sub_sample
